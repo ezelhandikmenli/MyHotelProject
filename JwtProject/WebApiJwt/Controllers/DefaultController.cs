a4dba6a7ef21aae22b7709805d316bb844b36482
Ezelhan Dikmenlioğlu <20060394@stu.omu.edu.tr>
2024-09-03 21:08:39 +0300

    eksiklerin giderilmesi

--- JwtProject/WebApiJwt/Controllers/DefaultController.cs
+++ JwtProject/WebApiJwt/Controllers/DefaultController.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using WebApiJwt.Models;
 
 namespace WebApiJwt.Controllers
@@ -9,18 +13,30 @@ namespace WebApiJwt.Controllers
     [ApiController]
     public class DefaultController : ControllerBase
     {
-                
         [HttpGet("[action]")]
-        public IActionResult Test()
+        public IActionResult TokenOlustur()
         {
             return Ok(new CreateToken().TokenCreate());
         }
 
-        [Authorize]
+        [HttpGet("[action]")]
+        public IActionResult AdminTokenOlustur()
+        {
+            return Ok(new CreateToken().TokenCreateAdmin());
+        }
+
+        [Authorize(Roles = "Admin")]
         [HttpGet("[action]")]
         public IActionResult Test2()
         {
             return Ok("Hoşgeldiniz");
+        }
+
+        [Authorize(Roles = "Admin,Visitor")]
+        [HttpGet("[action]")]
+        public IActionResult Test3()
+        {
+            return Ok("Token Başarılı Bir Şekilde Giriş Yaptı");
         }
     }
 }
